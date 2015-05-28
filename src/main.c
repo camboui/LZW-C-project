b078038764dadc18b6891dd2002ac6dd93d5296e
camboui <mathias.branger@gmail.com>
2015-05-28 09:17:35 +0200

    implementing main

--- src/main.c
+++ src/main.c
@@ -11,6 +11,22 @@
 
 int main (int argc, char *argv[])
 {
+
+	if (argc <=3)
+	{
+		if(strcmp(argv[],"-c"))
+		{
+			printf("\n\nCompression ");
+		}
+		else if(strcmp(argv[],"-d"))
+		{
+			printf("\n\nDecompression");
+		}
+	}
+	else
+	{
+		printf("\n\nErreur d'utilisation : ./lzw [-c/-d] [fichier]");
+	}
 	
 	return 0;
 }
