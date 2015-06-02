3958ab35dba850d72046baa87415a666db5a745f
camboui <mathias.branger@gmail.com>
2015-06-02 13:33:07 +0200

    add files

---
+++ include/RLE.h
@@ -0,0 +1,14 @@
+/*	POLYTECH Grenoble - RICM3 2015- PLS
+	David BUI
+	Yohann MATTEO
+	Mathias BRANGER
+	Florian PIERRE-LOUIS
+*/
+#ifndef STRUCTURE_DICO_H
+#define STRUCTURE_DICO_H
+
+int rle(char * nom_entree);
+
+int decoder_rle(char * nom_entree);
+
+#endif
